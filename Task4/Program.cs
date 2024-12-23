bf46444596da44d8bcc54a861a76c9533af081b5
Ilya_Dubinin <ilya_k-somon96@mail.ru>
2024-12-23 15:54:59 +0800

    нахождение максимального из 5и чисел

---
+++ Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 5;
+int b = 7;
+int c = 3;
+int d = 21;
+int e = 1;
+
+int max = a;
+
+if (b>max)
+{
+    max = b;
+}
+if (c>max)
+{
+    max = c;
+}
+if (d>max)
+{
+    max = d;
+}
+if (e>max)
+{
+    max = e;
+}
+Console.WriteLine (max);
